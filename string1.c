503f70046879c7356224ce4001ba3fda723eb824
EmmanuelJunior <142091756+EmmanuelJunuior@users.noreply.github.com>
2023-11-15 22:03:14 +0200

    Add files via upload

---
+++ string1.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[x])
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	dest[x] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - duplicates string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ *_puts - prints a string
+ *@str: string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: character to print
+ *
+ * Return: success 1.
+ * error, -1 is returned, errno set appropriately.
+ */
+int _putchar(char c)
+{
+	static int x;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || x >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, x);
+		x = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[x++] = c;
+	return (1);
+}
