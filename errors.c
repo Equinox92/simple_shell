50b1af59ce9f94c3c0c273d898afe24c74ebd3f6
Equinox92 <46786524+Equinox92@users.noreply.github.com>
2023-11-15 23:08:25 +0200

    errors commit

---
+++ errors.c
@@ -0,0 +1,82 @@
+#include "shell.h"
+
+/**
+ *_eputs - print input str
+ * @str: str to print
+ * Return: Nil
+ */
+void _eputs(char *str)
+{
+	int a = 0;
+
+	if (!str)
+		return;
+	while (str[a] != '\0')
+	{
+		_eputchar(str[a]);
+		a++;
+	}
+}
+
+/**
+ * _eputchar - write char c to std
+ * @c: The character to print
+ * Return: 1 (success) else
+ * -1, and errno is set.
+ */
+int _eputchar(char c)
+{
+	static int a;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, a);
+		a = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[a++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - write char c to fd
+ * @fd: fd to write to
+ * @c: char to print
+ * Return: On success 1 else
+ * -1, and errno is set.
+ */
+int _putfd(char c, int fd)
+{
+	static int a;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, a);
+		a = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[a++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - print an input str
+ * @fd: fd to write to
+ * @str: str to print
+ * Return: number of chars
+ */
+int _putsfd(char *str, int fd)
+{
+	int a = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		a += _putfd(*str++, fd);
+	}
+	return (a);
+
+}
